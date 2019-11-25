0a9414d8ce6e648363b44c83f57665be9073bc1d
Circle <>
2019-11-25 11:21:59 +0800

    Update samhook.go

--- samhook.go
+++ samhook.go
@@ -3,6 +3,7 @@ package samhook
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
 )
 
@@ -27,6 +28,25 @@ func Send(url string, msg Message) error {
 	body := bytes.NewReader(payloadBytes)
 
 	req, err := http.NewRequest(http.MethodPost, url, body)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+
+	return nil
+}
+
+// SendReader 發送message
+func SendReader(url string, r io.Reader) error {
+
+	req, err := http.NewRequest(http.MethodPost, url, r)
 	if err != nil {
 		return err
 	}
